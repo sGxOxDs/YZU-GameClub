2d0c110030b65b079d4801377ffb8930ab4fe822
sGxOxDs <62545840+sGxOxDs@users.noreply.github.com>
2021-11-23 11:57:54 +0800

    加入社課教學用專案
    模塊化的移動系統
    簡易碰撞

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 local = transform.localPosition;
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("w press");
+            transform.position = Vector3.Lerp(local, local + Vector3.forward, speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("a press");
+            transform.position = Vector3.Lerp(local, local + Vector3.left, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("s press");
+            transform.position = Vector3.Lerp(local, local + Vector3.back, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("d press");
+            transform.position = Vector3.Lerp(local, local + Vector3.right, speed * Time.deltaTime);
+        }
+    }
+
+}
